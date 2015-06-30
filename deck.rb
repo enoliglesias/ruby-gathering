d18a246d9a51439d29867bf490eec4d9a3bede5a
Enol Iglesias <enoliglesias@gmail.com>
2015-07-01 01:05:29 +0200

    Modify deck to generate card, hand and showcase

--- deck.rb
+++ deck.rb
@@ -1,6 +1,20 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+Squib::Deck.new(cards: 1, layout: %w(hand.yml even-bigger.yml)) do
+  background color: '#F05927'
+  deck = xlsx file: 'even-bigger.xlsx'
+  png file: deck['Art'], layout: 'Art'
+
+  %w(Title Description Snark).each do |key|
+    text str: deck[key], layout: key
+  end
+
+  %w(Attack Defend Health).each do |key|
+    svg file: "#{key.downcase}.svg", layout: "#{key}Icon"
+    text str: deck[key].first.to_i, layout: key
+  end
+
+  save_png prefix: 'card_'
+  showcase file: 'showcase.png', fill_color: '#0000'
+  hand file: 'hand.png', trim: 37.5, trim_radius: 25, fill_color: '#0000'
 end
